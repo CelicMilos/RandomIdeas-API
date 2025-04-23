b8baa05529e0f7156aa7c857e1f76a4fe5ac2643
CelicMilos <milos.celic.b@gmail.com>
2025-04-23 13:45:53 +0200

    backend work done

--- routes/ideas.js
+++ routes/ideas.js
@@ -2,63 +2,87 @@ const express = require("express");
 //express.Router() je Express.js alat koji omogucava organizovanje ruta u manje modularne delove
 //tj. posebne fajlve.Fajl se exportuje o pozove u glavni .js fajl sa require(../imefaja.js)
 const router = express.Router();
-
-const ideas = [
-  {
-    id: 1,
-    text: "Positive Newsletter,a newsletter that only shares positive,uplifting news",
-    tag: "Technology",
-    username: "Clem Fandango",
-    date: "2025-04-15",
-  },
-  {
-    id: 2,
-    text: "Milk cartons that turn a different color the lder that your milk is getting",
-    tag: "Inventions",
-    username: "Aurelio Buendia",
-    date: "2025-04-15",
-  },
-  {
-    id: 3,
-    text: "ATM location app wicth lets you know where the closest ATm is and if it is in service",
-    tag: "Softwere",
-    username: "Aurelio Buendia",
-    date: "2025-04-15",
-  },
-];
+const Idea = require("../models/Idea");
 
 //gat all ideas
-router.get("/", (request, response) => {
+router.get("/", async (request, response) => {
+  try {
+    const ideas = await Idea.find(); //.find() je asinhron,vraca obecanje
+    response.json({ success: true, data: ideas });
+  } catch (error) {
+    console.log(error);
+    response
+      .status(500)
+      .json({ succes: false, error: "Something went wrong." });
+  }
   //Moze ,.json umesto .send ali se onda podrazumeva JSON
   //i samo (ideas) ali bolje je kao objekat sa succes i data
-  response.json({ success: true, data: ideas });
 });
 //single idea - with query params-request.params.id
 // a u postmanu ili u browseru se dodaje /id
-router.get("/:id", (request, response) => {
-  const idea = ideas.find((idea) => idea.id === +request.params.id); //posto vraca string, + je ovde parsuje u broj
-
-  if (!idea) {
-    return response
-      .status(404)
-      .json({ success: false, error: "Resource not found" });
+router.get("/:id", async (request, response) => {
+  try {
+    const idea = await Idea.findById(request.params.id); //.findById-mongoose metod
+    response.json({ success: true, data: idea });
+  } catch (error) {
+    console.log(error);
+    response
+      .status(500)
+      .json({ success: false, error: "Something went wrong" });
   }
-  response.json({ success: true, data: idea });
 });
 
 //Post idea router.post('endpoint',function)
-router.post("/", (request, response) => {
-  //prvo mora da se intalira body parser u galvnom .js fajlu
+router.post("/", async (request, response) => {
+  //prvo mora da se instalira body parser u galvnom .js fajlu
   //i da bi se ideja zadrzala,mora da sepoveze na bazu
-  const idea = {
-    id: ideas.length + 1,
+  const idea = new Idea({
     text: request.body.text,
     tag: request.body.tag,
-    username: request.body.username, //obicno se pre ovoga obavi atentifikacija korisnika
-    date: new Date().toISOString().slice(0, 10),
-  };
-  ideas.push(idea);
-  response.json({ success: true, data: idea });
+    username: request.body.username, //obicno se pre ovoga obavi autentifikacija korisnika
+  });
+  try {
+    const savedIdea = await idea.save();
+    response.json({ success: true, data: savedIdea });
+  } catch (error) {
+    console.log(error);
+    response
+      .status(500)
+      .json({ success: false, error: "Something went wrong" });
+  }
+});
+//Update idea
+router.put("/:id", async (request, response) => {
+  try {
+    const updatedIdea = await Idea.findByIdAndUpdate(
+      //isto mongoose metod
+      request.params.id,
+      {
+        $set: {
+          text: request.body.text,
+          tag: request.body.tag,
+        },
+      },
+      { new: true }
+    );
+    response.json({ success: true, data: updatedIdea });
+  } catch (error) {
+    console.log(error);
+    response
+      .status(500)
+      .json({ success: false, error: "Something went wrong" });
+  }
+});
+
+//Delete Idea
+router.delete("/:id", async (request, response) => {
+  try {
+    await Idea.findByIdAndDelete(request.params.id);
+    response.json({ succes: true, data: {} });
+  } catch (error) {
+    console.log(error);
+    response.status(500).json({ succes: false, error: "Something went wrong" });
+  }
 });
 
 module.exports = router;
